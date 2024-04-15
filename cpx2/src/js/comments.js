081998c806a8601c916947b8b6637d32dd9c14a7
EnzzoMont <143364496+EnzzoMont@users.noreply.github.com>
2024-04-15 10:37:05 -0300

    Add files via upload

---
+++ cpx2/src/js/comments.js
@@ -0,0 +1,12 @@
+export const comments = [
+    {
+        nome: 'Steaphanie',
+        foto: '../assets',
+        cidade: 'Mauá, Brasil'
+    },
+    {
+        nome: 'Leandro',
+        foto: '../assets',
+        cidade: 'Brasil'
+    },
+]
